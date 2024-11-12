c888c0451dc502813d185e699e82561adccb7b1c
JonghyeokNam <njh9750@naver.com>
2024-11-12 15:47:29 +0900

    24/11/10 AppConfig를 통한 객체 지향 원리 적용

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,37 @@
+package hello.core;
+
+import hello.core.Member.MemberRepository;
+import hello.core.Member.MemberService;
+import hello.core.Member.MemberServiceImpl;
+import hello.core.Member.MemoryMemberRepository;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 애플리케이션의 설정정보
+public class AppConfig {
+
+    @Bean // 스프링 컨데이너에 등록
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
