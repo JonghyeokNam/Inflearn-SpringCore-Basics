c6a23c36cb5d29595d968295c190675bc101444c
JonghyeokNam <njh9750@naver.com>
2025-04-13 23:05:52 +0900

    섹션 8 컴포넌트 스캔 완강

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan( // @Component가 붙은 클래스를 다 찾아 자동 등록
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 여기 것들은 제외
+)
+public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+
+
+}
